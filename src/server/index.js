eef04a5c2d76a2c4fb1e024fc5c84ad56c4b4ce8
Nicole Hofstetter <nicole.hofstetter@audi.de>
2022-06-26 16:03:30 +0200

    chore: setup eslint and remove unused reference to css files

--- src/server/index.js
+++ src/server/index.js
@@ -1,49 +1,49 @@
 const dotenv = require('dotenv');
 dotenv.config();
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
-var bodyParser = require('body-parser')
-var cors = require('cors')
+require('path');
+const express = require('express');
+require('./mockAPI.js');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 var FormData = require('form-data');
-var fetch = require('node-fetch')
+var fetch = require('node-fetch');
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 // to use json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // to use url encoded values
 app.use(bodyParser.urlencoded({
     extended: true
-}))
+}));
 
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 
-console.log(__dirname)
+console.log(__dirname);
 
 app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
-})
+    res.sendFile('dist/index.html');
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
-})
+    console.log('Example app listening on port 8081!');
+});
 
 app.post('/classify', async function (req, res) {
     let inputText = req.body.text;
     let responseObject = await postSentimentApi(inputText);
-    res.send(responseObject)
-})
+    res.send(responseObject);
+});
 
 
 async function postSentimentApi(text) {
-    let apiKey = process.env.API_KEY
+    let apiKey = process.env.API_KEY;
 
     let formData = new FormData();
-    formData.append("key", apiKey);
-    formData.append("lang", "en");
-    formData.append("txt", text);
+    formData.append('key', apiKey);
+    formData.append('lang', 'en');
+    formData.append('txt', text);
 
     return await fetch('https://api.meaningcloud.com/sentiment-2.1', {
         method: 'POST',
@@ -52,10 +52,10 @@ async function postSentimentApi(text) {
     })
         .then(res => res.json())
         .then(function (res) {
-            return mapResponse(res)
+            return mapResponse(res);
         }).catch((error) => {
-            console.error("Error calling external api" + error)
-        })
+            console.error('Error calling external api' + error);
+        });
 }
 
 
@@ -71,11 +71,11 @@ function mapResponse(res) {
 
     let polarity = polarityMap.get(res.score_tag);
     let subjectivity = res.subjectivity.toLowerCase();
-    let irony = res.irony.toLowerCase()
+    let irony = res.irony.toLowerCase();
     let mappedResponse = {
         polarity: polarity,
         subjectivity: subjectivity,
         irony: irony
     };
-    return mappedResponse
+    return mappedResponse;
 }
