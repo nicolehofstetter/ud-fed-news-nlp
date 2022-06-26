75e4b18379c92d61a8c331458706fe3243d20378
Nicole Hofstetter <nicole.hofstetter@audi.de>
2022-06-26 13:39:56 +0200

    fix: Fix server & functionality like in lesson 4

--- src/server/index.js
+++ src/server/index.js
@@ -3,8 +3,17 @@ dotenv.config();
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+var bodyParser = require('body-parser')
+var cors = require('cors')
 
 const app = express()
+app.use(cors())
+// to use json
+app.use(bodyParser.json())
+// to use url encoded values
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
 app.use(express.static('dist'))
 
@@ -15,8 +24,8 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
